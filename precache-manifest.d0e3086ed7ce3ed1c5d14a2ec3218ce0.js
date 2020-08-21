588c358858178698eb940ebd608654ce21bd3763
ayush.singh <ayush.singh@comviva.com>
2020-08-22 01:17:59 +0530

    Updates

---
+++ precache-manifest.d0e3086ed7ce3ed1c5d14a2ec3218ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112da011be5bfd5b0b861086ef7076d0",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "25049cbe1e81ab120d25",
+    "url": "/Blog/static/css/main.9ee3d2f7.chunk.css"
+  },
+  {
+    "revision": "6e80193602908b49eb18",
+    "url": "/Blog/static/js/2.1df03e84.chunk.js"
+  },
+  {
+    "revision": "25049cbe1e81ab120d25",
+    "url": "/Blog/static/js/main.a835e56f.chunk.js"
+  },
+  {
+    "revision": "10f75463d09aa796d344",
+    "url": "/Blog/static/js/runtime~main.89c4d358.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "03db635616e1e87df4d9f08360b0bdd9",
+    "url": "/Blog/static/media/profile.03db6356.jpg"
+  }
+]);
